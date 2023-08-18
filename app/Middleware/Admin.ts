94c5502dc48977926cc478850355fb16162345e1
rononoo <140124125+rononoo@users.noreply.github.com>
2023-08-18 17:09:01 +0000

    update

--- app/Middleware/Admin.ts
+++ app/Middleware/Admin.ts
@@ -4,8 +4,25 @@ export default class Admin {
   public async handle({ auth, response }: HttpContextContract, next: () => Promise<void>) {
     // code for middleware goes here. ABOVE THE NEXT CALL
 
-    if (auth.user?.role !== 'admin') return response.redirect('/unauthorized')
+    // Log the auth object to see its properties
+    console.log('auth object:', auth);
 
-    await next()
+    // Check if the user is logged in
+    if (!auth.isLoggedIn) {
+      console.log('User is not logged in');
+      return response.redirect('/admin/login'); // Redirect to login if not logged in
+    }
+
+    // Log the authenticated user (for debugging)
+    console.log('Authenticated user:', auth.user);
+
+    // Check user's role
+    if (auth.user?.role !== 'admin') {
+      console.log('User is not an admin');
+      return response.redirect('/unauthorized');
+    }
+
+    // Continue to the next middleware
+    await next();
   }
 }
