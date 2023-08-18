5ae0b455c8bef033a91a92acdc4c80f9f33947e9
rononoo <140124125+rononoo@users.noreply.github.com>
2023-08-18 17:41:18 +0000

    update

--- app/Middleware/Admin.ts
+++ app/Middleware/Admin.ts
@@ -3,24 +3,10 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 export default class Admin {
   public async handle({ auth, response }: HttpContextContract, next: () => Promise<void>) {
     // code for middleware goes here. ABOVE THE NEXT CALL
-
-    // Log the auth object to see its properties
-    console.log('auth object:', auth);
-
-    // Check if the user is logged in
-    if (!auth.isLoggedIn) {
-      console.log('User is not logged in');
-      return response.redirect('/admin/login'); // Redirect to login if not logged in
-    }
-
-    // Log the authenticated user (for debugging)
-    console.log('Authenticated user:', auth.user);
+    const user = await auth.use('web').authenticate()
 
     // Check user's role
-    if (auth.user?.role !== 'admin') {
-      console.log('User is not an admin');
-      return response.redirect('/unauthorized');
-    }
+    if (user?.role !== 'admin') return response.redirect('/unauthorized');
 
     // Continue to the next middleware
     await next();
